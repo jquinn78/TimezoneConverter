9999a2815f09baafc9f6607c10ac41ed326634ee
jquinn78 <jquinn78@netzero.com>
2016-04-25 13:31:35 -0400

    Improved help, implemented ability to save output to csv, and normalized user input to RFC822Z format.

--- main.go
+++ main.go
@@ -4,20 +4,63 @@ Name: Timezone Converter
 Input: System time or user specifed time
 Output: Converted times
 
-TODO: improve the help
-      add support for multiple inputs
-	  add support to output the results to a csv file
-	  add automation of ZONEINFO environment variable
 */
 
 package main
 
 import (
+	"encoding/csv"
 	"flag"
 	"fmt"
+	"os"
 	"strings"
 	"time"
 )
+
+//save the times and converstions to a csv file
+func createCSV(timeInput string, tzInput string, sTime string) {
+
+	file, _ := os.Create("results.csv")
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+
+	headers := []string{"Time", "Converted Time"}
+
+	writer.Write(headers)
+
+	splitTime := strings.Split(timeInput, ",")
+	splitTimezone := strings.Split(tzInput, ",")
+
+	//Iterate through the user inputed time(s) or system time and
+	//save them and the converted times to results.csv.
+	for _, times := range splitTime {
+
+		date, _ := time.Parse(time.RFC822Z, times)
+
+		for _, timezones := range splitTimezone {
+
+			location, _ := time.LoadLocation(timezones)
+
+			if times != "" && timezones != "" {
+
+				writer.Write([]string{times, date.In(location).Format(time.RFC822)})
+			} else if times == "" && timezones != "" {
+
+				date, _ := time.Parse(time.RFC822Z, sTime)
+				writer.Write([]string{sTime, date.In(location).Format(time.RFC822)})
+			} else if times == "" && timezones == "" {
+				writer.Write([]string{sTime})
+			} else if times != "" && timezones == "" {
+				writer.Write([]string{times, "No time zone specified"})
+			}
+		}
+
+	}
+
+	defer writer.Flush()
+
+}
 
 //get time(s) from user
 func parsetime(timeInput string, tzInput string, sTime string) {
@@ -28,7 +71,6 @@ func parsetime(timeInput string, tzInput string, sTime string) {
 	for _, inputs := range test {
 
 		date, _ := time.Parse(time.RFC822Z, inputs)
-		dFormat := date.Format(time.RFC822)
 
 		if inputs == "" {
 
@@ -36,7 +78,8 @@ func parsetime(timeInput string, tzInput string, sTime string) {
 
 		} else {
 
-			fmt.Println("\n" + "Inputed Time:" + dFormat)
+			fmt.Println("\n" + "Inputted Time:" + date.Format(time.RFC822Z))
+
 		}
 
 		for _, tz := range test1 {
@@ -63,15 +106,20 @@ func main() {
 
 	t := time.Now()
 
-	tFormat := t.Format(time.RFC822)
+	tFormat := t.Format(time.RFC822Z)
 
 	//Define flags
-	inputFlag := flag.String("time", "", "Input the times to convert (format: 02 Jan 06 15:04 -0700)")
-	timezoneFlag := flag.String("timezone", "", "Enter the time zones")
+	inputFlag := flag.String("time", "", "Input the times to convert (format: 02 Jan 06 15:04 -0700). See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for the offsets.")
+	timezoneFlag := flag.String("timezone", "", "Enter the time zones. The timezones must be in the form of Country/City (ex. America/Chicago). See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for the IANA conversions.")
+	csvFlag := flag.Bool("csv", false, "Save data to csv. Append -csv to the end to save the inputted times and converted times to a CSV file.")
 
 	//Parse the flags
 	flag.Parse()
 
 	parsetime(*inputFlag, *timezoneFlag, tFormat)
+
+	if *csvFlag == true {
+		createCSV(*inputFlag, *timezoneFlag, tFormat)
+	}
 
 }
